621ca0aece650b91eefdc65b49aa90c9d8b0387f
mregorova <69008784+mregorova@users.noreply.github.com>
2023-11-28 20:58:36 +0300

    SNR works better

--- Hometask4/DSP2.m
+++ Hometask4/DSP2.m
@@ -8,7 +8,7 @@ N = 1:128;
 
 SignalSpec(5) = 1;
 SignalSpec(10) = 2;
-SignalSpec(60) = 3; %max frequency, по т Котельникова частота дискретизации должна быть около 120 у.е.
+SignalSpec(60) = 3;
 
 Signal = ifft(SignalSpec, 128);
 
@@ -21,6 +21,14 @@ Signal_im_spec = fft(Signal_im);
 figure;
 plot(N, abs(Signal))
 xlabel('Time'); ylabel('Amplitude'); title('Signal modulus');
+
+figure;
+plot(N, real(Signal_re_spec))
+xlabel('Time'); ylabel('Amplitude'); title('Signal real');
+
+figure;
+plot(N, imag(Signal_im_spec))
+xlabel('Time'); ylabel('Amplitude'); title('Signal im');
 
 %% Task 2
 
@@ -40,7 +48,7 @@ SNR_analyt = 10*log10(P_Signal/P_Noise);
 NoiseSpec = fft(Noise);
 NoisedSignalSpec = fft(NoisedSignal);
 
-P_SignalSpec = PowerSignal(SignalSpec) / 128;
+P_SignalSpec = PowerSignal(SignalSpec) / 128;   %нормировка
 P_NoiseSpec = PowerSignal(NoiseSpec) / 128;
 P_NoisedSignalSpec = PowerSignal(NoisedSignalSpec) / 128;
 
@@ -52,28 +60,27 @@ end
 
 %% Task 5
 
-%FilteredNoisedSignal = FilterSignal0(NoisedSignal, 15, 55, 1);
-FilteredNoisedSignal = FilterSignal(NoisedSignal, 15, 55, 12);
+FilteredNoisedSignal = FilterSignal(NoisedSignal, 15, 55);
 
 %% Task 6
 
 P_FilteredNoisedSignal = PowerSignal(FilteredNoisedSignal);
 
 SNR_noised = 10*log10(P_Signal / P_Noise);
-SNR_filtered = 10*log10(P_Signal / PowerSignal(FilteredNoisedSignal - Signal));
+SNR_filtered = 10*log10(PowerSignal(FilteredNoisedSignal) / P_Noise);
 
 Sum = 0;
 for itter = 1 : 50
     rng(itter)
     [NoisedSignal_i, Noise_i] = NoiseGenerator(Signal, SNR);
-    FilteredNoisedSignal_i = FilterSignal0(NoisedSignal_i, 15, 55, 12);
+    FilteredNoisedSignal_i = FilterSignal(NoisedSignal_i, 15, 55);
     
     P_NoisedSignal_i = PowerSignal(NoisedSignal_i);
     P_FilteredNoisedSignal_i = PowerSignal(FilteredNoisedSignal_i);
     P_Noise_i = PowerSignal(Noise);
 
     SNR_noised_i = 10*log10(P_NoisedSignal_i / P_Noise_i);
-    SNR_filtered_i = 10*log10(P_FilteredNoisedSignal_i / PowerSignal(FilteredNoisedSignal_i - Signal));
+    SNR_filtered_i = 10*log10(PowerSignal(FilteredNoisedSignal_i) / P_Noise_i);
 
     Sum = Sum + SNR_filtered_i - SNR_noised_i;
 end
@@ -86,12 +93,12 @@ delta(1:60) = 0;
 for itter = 1 : 60
     rng(SNR)
     [NoisedSignal_i, Noise_i] = NoiseGenerator(Signal, x_SNR(itter));
-    FilteredNoisedSignal_i = FilterSignal(NoisedSignal_i, 15, 55, 12);
+    FilteredNoisedSignal_i = FilterSignal(NoisedSignal_i, 15, 55);
 
     P_Noise_i = PowerSignal(Noise_i);
 
     SNR_noised_i = 10*log10(P_Signal / P_Noise_i);
-    SNR_filtered_i = 10*log10(P_Signal / PowerSignal(FilteredNoisedSignal_i - Signal));
+    SNR_filtered_i = 10*log10(PowerSignal(FilteredNoisedSignal_i) / P_Noise_i);
 
     delta(itter) = SNR_filtered_i - SNR_noised_i;
 end
@@ -101,4 +108,7 @@ plot(x_SNR, delta);
 xlabel('SNR'); ylabel('SNR filtered[i] - SNR noised[i]'); 
 title('Dependence delta');
 
-fprintf("Gragh demostrates that SNR filtered is worse than SNR noised(((\n");
+fprintf("Filtered signal: %1.3f\n" + ...
+    "Noised signal: %1.3f\n" + ...
+    "Delta: %1.3f\n", ...
+    SNR_filtered, SNR_noised, SNR_filtered - SNR_noised);
