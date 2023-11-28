322c5aadd698729b7a4511fe6bbccf5b21cf0de0
mregorova <69008784+mregorova@users.noreply.github.com>
2023-11-28 03:19:01 +0300

    Add files via upload

--- Hometask 3/Task4.m
+++ Hometask 3/Task4.m
@@ -1,6 +1,6 @@
 clear; clc; close all;
 
-[signal, fd] = audioread('voice.wav');
+[signal, fd] = audioread('voice2.wav');
 
 signal(signal==0) = [];
 signal = log10(abs(signal));
@@ -21,10 +21,18 @@ faxis = linspace(-fd/2, fd/2, length(signal));
 
 figure;
 plot(faxis, fftshift(abs(signal_filtered_spec)))
-xlabel('k'); ylabel('|X(n)|'); title('Signal Spec (new f axis)');
+xlabel('Hz'); ylabel('|X(n)|'); title('Signal Spec filtered');
 
 [~, idx_ft] = max(abs(signal_filtered_spec(length(signal)/2:end)));
 ft = abs(faxis(idx_ft));
 
-% voice tone is about 546.85 Hz
+% voice tone is about 600.57 Hz
+
+Fs = 2500; % discretization frequency
+t = 0:1/Fs:3; %time
+f = 600; % sin frequency
+
+x = sin(2*pi*f*t); % sinusoida
+
+audiowrite("mysin.wav", x, Fs);
 
