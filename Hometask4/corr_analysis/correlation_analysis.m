a27150c3496cda965dbdce6174fa4701c36d70ee
mregorova <69008784+mregorova@users.noreply.github.com>
2023-11-28 18:23:03 +0300

    Fixed some issues

--- Hometask4/corr_analysis/correlation_analysis.m
+++ Hometask4/corr_analysis/correlation_analysis.m
@@ -2,21 +2,26 @@ clear; clc; close all;
 load('Matlab_L3_6.mat')
 
 len = 128; %заданная длина М-последовательности
+eps = 0.001;
 
-g_poly1 = [1 0 0 0 0 1]; %полином первого генератора
-g_gen1  = [0 1 0 1 0 1]; %начальное состояние регистров первого генератора
-g_poly2 = [1 1 0 0 1 1]; %полином второго генератора
-g_gen2  = [0 1 0 0 1 1]; %начальное состояние регистров второго генератора
-m_poly  = [1 1 0 0 1 1]; %полином генератора М-последовательности
-m_init  = [0 1 0 0 1 1]; %начальное состояние генератора М-последовательности
+g_poly1 = [1 1 1 1 0 0 1 1]; %полином первого генератора 
+g_gen1  = [1 0 0 1 0 1 1 1]; %начальное состояние регистров первого генератора
+g_poly2 = [1 1 1 0 0 0 0 1]; %полином второго генератора 
+g_gen2  = [1 1 0 0 1 0 1 1]; %начальное состояние регистров второго генератора
+m_poly  = [1 1 1 1 0 0 1 1]; %полином генератора М-последовательности
+m_init  = [1 0 0 1 0 1 1 1]; %начальное состояние генератора М-последовательности
 
 m_seq = MSeq(m_init, m_poly, len); %генерация М-последовательности
 gold_seq_1 = MSeq(g_gen1, g_poly1, len);
 gold_seq_2 = MSeq(g_gen2, g_poly2, len);
 gold_seq = xor(gold_seq_1, gold_seq_2);
 
-[Data_Length_1, Frame_Length_1, Start_Of_Frame_Position_1, Number_Of_Frames_1] = propers(Bit_Stream_1, gold_seq, len);
-[Data_Length_2, Frame_Length_2, Start_Of_Frame_Position_2, Number_Of_Frames_2] = propers(Bit_Stream_2, m_seq, len);
+xcorr_gold = autocor(gold_seq);
+%xcorr_gold = xcorr_array_1 / max(xcorr_array_1);
+xcorr_m = autocor(m_seq);
+
+[Data_Length_1, Frame_Length_1, Start_Of_Frame_Position_1, Number_Of_Frames_1] = propers(xcorr_gold, length(gold_seq), length(Bit_Stream_1), 1, eps);
+[Data_Length_2, Frame_Length_2, Start_Of_Frame_Position_2, Number_Of_Frames_2] = propers(xcorr_gold, length(gold_seq), length(Bit_Stream_2), 1, eps);
 
 fprintf("Data_Length of Gold seq = %d\n", Data_Length_1(end));
 fprintf("Frame_Length of Gold seq = %d\n", Frame_Length_1(end));
@@ -46,15 +51,18 @@ grid on;
 
 savefig("Frame_Corr.fig");
 
+figure;
+[autocorry_gold, autocorrx_gold] = xcorr(gold_seq);
 subplot(2, 1, 1)
-plot(autocor(gold_seq))
+plot(autocorrx_gold, autocorry_gold)
 title("Автокорреляция последовательноти кода Голда от номера бита");
 ylabel("Корреляция");
 xlabel("Смещение");
 grid on;
 
+[autocorry_m, autocorrx_m] = xcorr(m_seq);
 subplot(2, 1, 2)
-plot(autocor(m_seq))
+plot(autocorrx_m, autocorry_m)
 title("Автокорреляция М-последовательности от номера бита");
 ylabel("Корреляция");
 xlabel("Смещение");
--- Hometask4/corr_analysis/propers.m
+++ Hometask4/corr_analysis/propers.m
@@ -1,26 +1,15 @@
-function [Data_Length, Frame_Length, Start_Of_Frame_Position, Number_Of_Frames] = propers(Bit_Stream, seq, len)
-    opt_len = floor(length(Bit_Stream) / 128) + 1;
-    Data_Length = zeros(1, opt_len - 1);
+function [Data_Length, Frame_Length, Start_Of_Frame_Position, Number_Of_Frames] = propers(xcorr, seq_len, stream_len, border_value, eps)
+
     Frame_Length = 0;
-    Start_Of_Frame_Position = zeros(1, opt_len);
-    Number_Of_Frames = 1;    
+    Start_Of_Frame_Position = zeros(1, round(stream_len / (2 * seq_len)));
+    Number_Of_Frames = 0;    
 
-    i = 1;
-    while i <= length(Bit_Stream) - (len - 1)
-        if sum(seq - Bit_Stream(i : i + len - 1)) == 0
-
-           Start_Of_Frame_Position(Number_Of_Frames) = i;
-           i = i + len - 1;
-
-           if Number_Of_Frames > 1
-            Data_Length(Number_Of_Frames - 1) = Start_Of_Frame_Position(Number_Of_Frames) - Start_Of_Frame_Position(Number_Of_Frames - 1) - (len - 1);
-           end
-
-           Number_Of_Frames = Number_Of_Frames + 1;
+    for i = 1 :length(xcorr) - (seq_len - 1)
+        if abs(xcorr(i) - border_value) < eps
+            Number_Of_Frames = Number_Of_Frames + 1;
+            Start_Of_Frame_Position(Number_Of_Frames) = i - seq_len - 1;
         end
-        
-        i = i + 1;
-        Frame_Length = Data_Length + len;
-
     end
+    Frame_Length = Start_Of_Frame_Position(2) - Start_Of_Frame_Position(1);
+    Data_Length = seq_len - Frame_Length;
 end
